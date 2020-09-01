ca51e2e5ec42b09073c14edebc25b21e4e760d2b
Pavlo Dubenko <d.pavka@gmail.com>
2020-09-01 22:59:12 +0300

    initial commit
    quick and dirty example

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {MainComponent} from './main/main.component';
+import {MenuComponent} from './menu/menu.component';
+import {LayoutComponent} from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+    path: '',
+    component: LayoutComponent,
+    children: [
+        {
+            path: '',
+            component: MainComponent
+        },
+        {
+            path: '',
+            outlet: 'menu',
+            component: MenuComponent
+        }
+    ]
+},
+
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { MainComponent } from './main/main.component';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { MenuComponent } from './menu/menu.component';
+import { LayoutComponent } from './layout/layout.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+import {MatButtonModule} from '@angular/material/button';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatCardModule} from '@angular/material/card';
+import {MatChipsModule} from '@angular/material/chips';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    MenuComponent,
+    LayoutComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NoopAnimationsModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatListModule,
+    MatButtonModule,
+    FlexLayoutModule,
+    MatCardModule,
+    MatChipsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
